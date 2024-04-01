9fc3f441b9e7bc33941460b6f3adf8ca3d94229d
baevar_laptop <baevar@mai.ru>
2024-04-01 11:08:25 +0700

    сделал HomeTask3 с решением через реверс строки

--- HomeTask3/Program.cs
+++ HomeTask3/Program.cs
@@ -2,7 +2,7 @@
 // является ли она палиндромом.
 
 
-bool IsStringPalindrome(string str)
+bool IsStringPalindrome(string str)             // решение просто сравниваем строку саму с собой
 {
     for (int i = 0; i < str.Length / 2; i++)
     {
@@ -13,7 +13,38 @@ bool IsStringPalindrome(string str)
 
 }
 
+string ReverseString(string str)                // разворачиваем строку
+{
+    string result = string.Empty;
+    for (int i = str.Length - 1; i >= 0; i--)
+    {
+    result += str[i];
+    }
+    return result;
+}
+
+bool IsSimilarTwoString(string str, string str2)         // решение сравниваем строку с ее реверсной копией
+{
+    for (int i = 0; i < str.Length -1; i++)
+    {
+        if (str[i] != str2[i])
+            return false;
+    }
+    return true;
+
+}
+
+
 Console.WriteLine("Введите строку палиндром");
+
 string str = Console.ReadLine();
+
+string result = ReverseString (str);
+
+Console.WriteLine("Метод сравнения строки и ее реверсной копии");
+
+Console.WriteLine(IsSimilarTwoString(str, result) ? "Строка палиндром" : "Строка не палиндром");
+
+Console.WriteLine("Метод сравнения строки самой с собой");
 
 Console.WriteLine(IsStringPalindrome(str) ? "Строка палиндром" : "Строка не палиндром");
